5dfef8abf5b29edcb40574a8c506873464cfa396
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-11-17 21:32:36 +0630

    filter by name,company,featured

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,6 +1,22 @@
+const Product = require("../models/product");
 const getAllProducts = async (req, res) => {
-  throw new Error("Test ERROR");
-  res.status(200).send("Get All Products");
+  // throw new Error("Test ERROR"); // auto throw to default error handler cause of express-aysnc-errors package
+  // const products = await Product.find(req.query); // return result according to query // nth return if some unexist field passed like jobs=web
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    // queryObject.name = name;  //only work on exact name
+    queryObject.name = { $regex: name, $options: "i" }; // options 'i' for case insensitive
+  }
+  //filter the field we want to be searched
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 module.exports = {
   getAllProducts,
--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -8,7 +8,7 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: [true, "Price must be provided"],
   },
-  feature: {
+  featured: {
     type: Boolean,
     default: false,
   },
