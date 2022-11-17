4cde5de69522b65d7eaf1fc4d04b9582548245ee
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-11-17 23:23:08 +0630

    add numericFilter to price & rating

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 const getAllProducts = async (req, res) => {
   // throw new Error("Test ERROR"); // auto throw to default error handler cause of express-aysnc-errors package
   // const products = await Product.find(req.query); // return result according to query // nth return if some unexist field passed like jobs=web
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilter } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -14,8 +14,34 @@ const getAllProducts = async (req, res) => {
     // queryObject.name = name;  //only work on exact name
     queryObject.name = { $regex: name, $options: "i" }; // options 'i' for case insensitive
   }
+  //numericFiltering
+  if (numericFilter) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$e",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b(<|<|<=|=|>=|>)\b/g;
+    let filter = numericFilter.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(filter);
+    const options = ["price", "rating"];
+    filter = filter.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }; //dynamic key []
+        // queryObject.field = { operator: Number(value) };
+      }
+    });
+    console.log(queryObject);
+  }
   //filter the field we want to be searched
-  let result = Product.find(queryObject);
+  let result = Product.find(queryObject).select("name price");
   //sorting
   if (sort) {
     //sort=name,-price => .sort('name -price)
@@ -28,9 +54,13 @@ const getAllProducts = async (req, res) => {
   if (fields) {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
-  } else {
-    result = result.select("name price");
   }
+
+  //limiting result
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
